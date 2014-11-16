1f66588d8c80d70cef98a70b63e0f7417ababad3
sashkevichi <kakhidze2012@gmail.com>
2014-11-17 00:43:43 +0400

    changed

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ var methodOverride=require("method-override");
 var cookieParser=require("cookie-parser");
 var stylus=require("stylus");
 var routes=require("./routes");
-var loginRoute=require("./routes/login");
-var usersRoute=require("./routes/users");
-var adminRoute=require("./routes/admin");
-var addTests=require("./routes/addTests");
-var deleteTest=require("./routes/deleteTest")
-var addQuestion=require("./routes/addQuestion");
-var getQuestion=require("./routes/getQuestion");
-var changeQuestion=require("./routes/changeQuestion");
-var deleteQuestion=require("./routes/deleteQuestion");
-var registerRoute=require("./routes/register");
+// var loginRoute=require("./routes/login");
+// var usersRoute=require("./routes/users");
+// var adminRoute=require("./routes/admin");
+// var addTests=require("./routes/addTests");
+// var deleteTest=require("./routes/deleteTest")
+// var addQuestion=require("./routes/addQuestion");
+// var getQuestion=require("./routes/getQuestion");
+// var changeQuestion=require("./routes/changeQuestion");
+// var deleteQuestion=require("./routes/deleteQuestion");
+// var registerRoute=require("./routes/register");
 var app=express();
 
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/Quizzes',function(err,data){
-	if(!err){
-		console.log("connected to database");
-	}
-	else{
-		return;
-	}
-});
+// var mongoose=require('mongoose');
+// mongoose.connect('mongodb://localhost/Quizzes',function(err,data){
+// 	if(!err){
+// 		console.log("connected to database");
+// 	}
+// 	else{
+// 		return;
+// 	}
+// });
 
-app.set('port',process.env.PORT||3000);
+app.set('port', (process.env.PORT || 5000))
 app.set('views',__dirname+'/views');
 app.set('view engine','jade');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -41,35 +41,32 @@ var router=express.Router();
 router
 	.route('')
 		.get(routes.index)
-		.post(registerRoute.register)
+// 		.post(registerRoute.register)
 
-router
-	.route('/users')
-		.get(usersRoute.users)
-		.post(loginRoute.login)
+// router
+// 	.route('/users')
+// 		.get(usersRoute.users)
+// 		.post(loginRoute.login)
 
 
-router
-	.route('/admin')
-		.get(adminRoute.admin)
-		.post(addTests.addTests)
-router
-	.route('/admin/question')
-		.get(getQuestion.getQuestion)
-		.post(addQuestion.addQuestion)
-		.put(changeQuestion.changeQuestion)
-		.delete(deleteQuestion.deleteQuestion)
+// router
+// 	.route('/admin')
+// 		.get(adminRoute.admin)
+// 		.post(addTests.addTests)
+// router
+// 	.route('/admin/question')
+// 		.get(getQuestion.getQuestion)
+// 		.post(addQuestion.addQuestion)
+// 		.put(changeQuestion.changeQuestion)
+// 		.delete(deleteQuestion.deleteQuestion)
 		
-router
-	.route('/admin/:test')
-		.delete(deleteTest.deleteTest)
+// router
+// 	.route('/admin/:test')
+// 		.delete(deleteTest.deleteTest)
 
 
 app.use("/",router);
 
-app.listen(3000,function(err,data) {
-	if(err){
-		console.log("error while connecting this port");
-	}
-	console.log("connected PORT: "+app.get('port'));
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
