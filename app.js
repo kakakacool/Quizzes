d1854470af5e88f47ed76ac12c35dde2b695d6bc
sashkevichi <kakhidze2012@gmail.com>
2014-11-17 01:54:42 +0400

    add DB

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ var methodOverride=require("method-override");
 var cookieParser=require("cookie-parser");
 var stylus=require("stylus");
 var routes=require("./routes");
-// var loginRoute=require("./routes/login");
-// var usersRoute=require("./routes/users");
-// var adminRoute=require("./routes/admin");
-// var addTests=require("./routes/addTests");
-// var deleteTest=require("./routes/deleteTest")
-// var addQuestion=require("./routes/addQuestion");
-// var getQuestion=require("./routes/getQuestion");
-// var changeQuestion=require("./routes/changeQuestion");
-// var deleteQuestion=require("./routes/deleteQuestion");
-// var registerRoute=require("./routes/register");
+var loginRoute=require("./routes/login");
+var usersRoute=require("./routes/users");
+var adminRoute=require("./routes/admin");
+var addTests=require("./routes/addTests");
+var deleteTest=require("./routes/deleteTest")
+var addQuestion=require("./routes/addQuestion");
+var getQuestion=require("./routes/getQuestion");
+var changeQuestion=require("./routes/changeQuestion");
+var deleteQuestion=require("./routes/deleteQuestion");
+var registerRoute=require("./routes/register");
 var app=express();
 
-// var mongoose=require('mongoose');
-// mongoose.connect('mongodb://localhost/Quizzes',function(err,data){
-// 	if(!err){
-// 		console.log("connected to database");
-// 	}
-// 	else{
-// 		return;
-// 	}
-// });
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://kaxi1993:kaxi@ds053090.mongolab.com:53090/quizzes',function(err,data){
+	if(!err){
+		console.log("connected to database");
+	}
+	else{
+		return;
+	}
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.set('views',__dirname+'/views');
@@ -41,28 +41,28 @@ var router=express.Router();
 router
 	.route('')
 		.get(routes.index)
-// 		.post(registerRoute.register)
+		.post(registerRoute.register)
 
-// router
-// 	.route('/users')
-// 		.get(usersRoute.users)
-// 		.post(loginRoute.login)
+router
+	.route('/users')
+		.get(usersRoute.users)
+		.post(loginRoute.login)
 
 
-// router
-// 	.route('/admin')
-// 		.get(adminRoute.admin)
-// 		.post(addTests.addTests)
-// router
-// 	.route('/admin/question')
-// 		.get(getQuestion.getQuestion)
-// 		.post(addQuestion.addQuestion)
-// 		.put(changeQuestion.changeQuestion)
-// 		.delete(deleteQuestion.deleteQuestion)
+router
+	.route('/admin')
+		.get(adminRoute.admin)
+		.post(addTests.addTests)
+router
+	.route('/admin/question')
+		.get(getQuestion.getQuestion)
+		.post(addQuestion.addQuestion)
+		.put(changeQuestion.changeQuestion)
+		.delete(deleteQuestion.deleteQuestion)
 		
-// router
-// 	.route('/admin/:test')
-// 		.delete(deleteTest.deleteTest)
+router
+	.route('/admin/:test')
+		.delete(deleteTest.deleteTest)
 
 
 app.use("/",router);
