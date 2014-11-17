4da4422bcd924faa5a46953eba09fcc14cd00196
sashkevichi <kakhidze2012@gmail.com>
2014-11-17 21:19:22 +0400

    add several questions

--- routes/addQuestion.js
+++ routes/addQuestion.js
@@ -1,43 +1,50 @@
 var mongoose=require('mongoose');
 var questionSchema=require('./schemaDB/question').questionSchema;
 var questionModel=mongoose.model("questions",questionSchema)
-
+var conn=mongoose.connection;
 exports.addQuestion=function (req,res) {
 	var questions=JSON.parse(req.body.arr);
 	var error=false;
+	var ansArr=[];
 	for(var i=0;i<questions.length;i++){
-		if(validate(questions[i])){
-			var question=questions[i];
-			var arr=question.answers;
-			var filterAns=[];
-			for(var i=0;i<arr.length-1;i++){
-				if(arr[i].trim()!=""){
-					filterAns.push(arr[i].trim())
+		validate(questions[i],function(err,question){
+			if(err){
+				console.log(err);
+			}else{
+				var arr=question.answers;
+				var filterAns=[];
+				for(var i=0;i<arr.length-1;i++){
+					question.answers[i]=question.answers[i].trim();
 				}
+				var arr=question.answers;
+				for(var i=0;i<arr.length-1;i++){
+					if(arr[i]!=""){
+						filterAns.push(arr[i])
+					}
+				}
+				filterAns.push(arr[arr.length-1])
+				var obj={
+					text:question.text.trim(),
+					answers:filterAns,
+					test:question.test
+				}
+				ansArr.push(obj);
 			}
-			filterAns.push(arr[arr.length-1])
-			new questionModel({
-				text:question.text.trim(),
-				answers:filterAns,
-				test:question.test
-			}).save(function(err,data){
-				if(err){
-					error=true;
-				}
-			})
-		}else{
-			error=true;
-		}
+		})
 	}
-	if(error){
-		res.json({success:false,text:"Some Question(s) not saved"});
-	}else{
-		res.json({success:true,text:"Question(s) saved"});
-	}	
+	conn.collection('questions').insert(ansArr,function(err,data){
+		console.log(data);
+		if(error||err){
+			res.json({success:false,text:"Some Question(s) not saved"});
+		}else{
+			res.json({success:true,text:"Question(s) saved"});
+		}	
+	})
 }
-function validate(obj){
+function validate(obj,callback){
+	var err=false;
 	if(obj.text==""){
-		return false;
+		err=true;
 	}
 	var arr=obj.answers;
 	var filterArr=[];
@@ -47,10 +54,10 @@ function validate(obj){
 		}
 	}
 	if(filterArr.length<2){
-		return false;
+		err=true;
 	}
 	if(arr[arr.length-1]==-1){
-		return false;
+		err=true;
 	}
-	return true;
+	callback(err,obj);
 }
