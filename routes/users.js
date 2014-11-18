fe77c50fa52de2e1d6aba9cfd862adb4888992d4
sashkevichi <kakhidze2012@gmail.com>
2014-11-18 10:25:00 +0400

    Production mode

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ exports.users=function(req,res) {
 	}else{
 		testsModel.find({},{_id:0,__v:0},function(err,data){
 			if(err||data.length==0){
-				res.render('user',{title:"No test wwas found"});
+				res.render('user',{title:"No test found"});
 			}else{
 				res.render('user',{title:"Quizzes",tests:data});
 			}
