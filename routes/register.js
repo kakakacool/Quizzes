7a26febda927dfcaa9737f4cc2669b78ca990894
sashkevichi <kakhidze2012@gmail.com>
2014-11-22 01:34:56 +0400

    bug fix

--- routes/register.js
+++ routes/register.js
@@ -5,15 +5,19 @@ var model=mongoose.model("users",userSchema);
 
 
 exports.register=function(req,res){
-	new model({
-		email:req.body.email,
-		password:req.body.password
-	}).save(function(err,data){
-		if(err){
-			res.json({success:false,text:"User with this email already exists!"}); 
-		}
-		else{
-			res.json({success:true,text:"Your registration was successfull"});	
-		}
-	})
+	if(req.body.email==""||req.body.password==""){
+
+	}else{
+		new model({
+			email:req.body.email,
+			password:req.body.password
+		}).save(function(err,data){
+			if(err){
+				res.json({success:false,text:"User with this email already exists!"}); 
+			}
+			else{
+				res.json({success:true,text:"Your registration was successfull"});	
+			}
+		})
+	}
 }
